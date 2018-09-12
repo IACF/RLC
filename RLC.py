313cd70bbe5f48d7e03b000659b9dd05931cd0e1
italo.andre2007 <italo.andre2007@gmail.com>
2018-09-12 18:30:50 -0300

    Falta só degrau e amortecimento em Série

--- RLC.py
+++ RLC.py
@@ -2,15 +2,19 @@ import ahkab
 import numpy as np
 import math
 from sympy import *
+import matplotlib.pyplot as plt
 
 # R = float(input('Entre com o Resistor: '))
 # C = float(input('Entre com o Capacitor: '))
+V0 = float(input('Entre com a tensão V(0) do Capacitor: '))
+I0 = float(input('Entre com a corrente I(0) do Indutor: '))
+
 # L = float(input('Entre com o Indutor: '))
 associacao = input('Entre com  \"\\\\" para RLC paralelo ou \"--\" para RLC série: ')
 
 m = 10**(-3) ##definicao de mili
-R = 1.923
-C = 10*m
+R = 5
+C = 0.25
 L = 1
 
 A1 = symbols('A1')
@@ -19,13 +23,16 @@ B1 = symbols('B1')
 B2 = symbols('B2')
 t = symbols('t')
 
-def resposta_rlc(alpha, omega, associacao, Vs, Is, s1, s2): #funcao para verificar tipo de resposta e retornar a resposta natural a partir do tipo de amortecimento
+def resposta_rlc(alpha, omega, associacao, Vs, Is, s1, s2, A1, A2): #funcao para verificar tipo de resposta e retornar a resposta natural a partir do tipo de amortecimento
 	resposta = ""
+	
 	if alpha > omega:
 		resposta = "supercrítico"
 		if(associacao == '\\\\'):
 			r = A1*exp(s1*t) + A2*exp(s2*t)
 			r_degrau = Is + A1*exp(s1*t) + A2*exp(s2*t)
+			print('ERRO S1',s1, A1)
+			print('ERRO S2',s2, A2)
 		elif (associacao == '--') :
 			r = A1*exp(s1*t) + A2*exp(s2*t)
 			r_degrau = Vs + A1*exp(s1*t) + A2*exp(s2*t)
@@ -52,7 +59,7 @@ def resposta_rlc(alpha, omega, associacao, Vs, Is, s1, s2): #funcao para verific
 if associacao == '--':
 	while(True):
 		try:
-			Vs = float (input('Entre com V(0) para obter a resposta ao degrau, ou 0 caso não queira: '))
+			Vs = float (input('Entre com Vss para obter a resposta ao degrau, ou 0 caso não queira: '))
 			break
 		except:
 			print('Entrada inválida')
@@ -62,8 +69,14 @@ if associacao == '--':
 	omega = 1./(sqrt(L*C))
 	s1 = -alpha + sqrt(alpha**2 - omega**2)
 	s2 = -alpha - sqrt(alpha**2 - omega**2)
+	print('v = ',V0 - Vs)
+	a = np.array([[1,1],[s1, s2]],dtype='float')
+	b = np.array([-20,16],dtype='float')
+	x = np.linalg.solve(a,b)
+	A1 = x[1]
+	A2 = x[0]
 
-	resposta,r,r_degrau = resposta_rlc(alpha, omega,associacao, Vs, 0)
+	resposta,r,r_degrau = resposta_rlc(alpha, omega,associacao, Vs, 0, s1, s2, A1, A2)
 
 	print("Alpha:",alpha)
 	print("Omega:",omega)
@@ -72,6 +85,12 @@ if associacao == '--':
 	print("Resposta:",resposta)
 	print("Resposta i(t):",r)
 	print("Resposta ao degrau i(t):",r_degrau)
+	tx = np.arange(0,5,0.1)
+	f = lambdify(t,r_degrau)
+	ty = f(tx)
+	plt.plot(tx,ty)
+	plt.show()
+	print(f(1))
 
 elif associacao == '\\\\':
 	
@@ -81,19 +100,19 @@ elif associacao == '\\\\':
 			break
 		except:
 			print('Entrada inválida')
-	
 	alpha = 1/(2*R*C)
 	omega = 1/(np.sqrt(L*C))
 	s1 = -alpha + sqrt(alpha**2 - omega**2)
 	s2 = -alpha - sqrt(alpha**2 - omega**2)
 
 	a = np.array([[1,1],[s1, s2]],dtype='float')
-	b = np.array([5,0],dtype='float')
+	b = np.array([V0,-((V0+R*Is)/R*C)],dtype='float')
 	x = np.linalg.solve(a,b)
-	A1 = x[0]
-	A2 = x[1]
+	A1 = x[1]
+	A2 = x[0]
 
-	resposta, r, r_degrau = resposta_rlc(alpha, omega, associacao, 0, Is, s1, s2)
+
+	resposta, r, r_degrau = resposta_rlc(alpha, omega, associacao, 0, Is, s1, s2, A1, A2)
 
 	print("Alpha:",alpha)
 	print("Omega:",omega)
@@ -102,3 +121,12 @@ elif associacao == '\\\\':
 	print("Amortecimento ",resposta)
 	print("Resposta v(t):",r)
 	print("Resposta ao degrau v(t):",r_degrau)
+	
+	tx = np.arange(0,5,0.1)
+	f = lambdify(t,r_degrau)
+	ty = f(tx)
+	plt.plot(tx,ty)
+	plt.show()
+	print(f(1))
+
+
